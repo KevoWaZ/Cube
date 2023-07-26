214a2a442d446499f446c776a097b36a1cc767c7
KevoWaZ <kevin.gauthier145@gmail.com>
2023-07-26 18:26:30 +0200

    Cube

--- app.js
+++ app.js
@@ -33,19 +33,44 @@ aire.onchange = function () {
 aireTotal.onchange = function () {
     let number = Number(aireTotal.value)
 
+    let newAire = number / 6
+    aire.value = newAire
+    let newArete = Math.sqrt(newAire)
+    arete.value = newArete
+    volume.value = aire.value * arete.value
+    diagFace.value = Math.sqrt(2) * arete.value
+    diagCube.value = Math.sqrt(arete.value * arete.value + Math.pow(diagFace.value, 2))
 }
 
 volume.onchange = function () {
-    let number = Number(diagonale.value)
+    let number = Number(volume.value)
 
+    let newArete = Math.cbrt(number)
+    arete.value = newArete
+    aire.value = arete.value * arete.value
+    aireTotal.value = aire.value * 6
+    diagFace.value = Math.sqrt(2) * arete.value
+    diagCube.value = Math.sqrt(arete.value * arete.value + Math.pow(diagFace.value, 2))
 }
 
 diagFace.onchange = function () {
-    let number = Number(diagonale.value)
+    let number = Number(diagFace.value)
 
+    let newArete = number / Math.sqrt(2)
+    arete.value = newArete
+    aire.value = arete.value * arete.value
+    aireTotal.value = aire.value * 6  
+    volume.value = aire.value * arete.value
+    diagCube.value = Math.sqrt(arete.value * arete.value + Math.pow(diagFace.value, 2))  
 }
 
 diagCube.onchange = function () {
-    let number = Number(diagonale.value)
+    let number = Number(diagCube.value)
 
+    let newArete = number / Math.sqrt(3)
+    arete.value = newArete
+    aire.value = arete.value * arete.value
+    aireTotal.value = aire.value * 6
+    volume.value = aire.value * arete.value
+    diagFace.value = Math.sqrt(2) * arete.value
 }
